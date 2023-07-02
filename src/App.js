84f4caf448d1f8393df2c38f7ab2529e759b5eed
Bill Franklin <franklinbk316@gmail.com>
2023-07-02 14:39:47 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,121 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Square from './Components/Square';
+import { Patterns } from './Patterns';
 
 function App() {
+  const [ board, setBoard ] = useState(['','','','','','','','','']);
+  const [ player, setPlayer ] = useState("O");
+  const [ result, setResult ] = useState({winner: "none", state: "none"})
+  
+  useEffect(() => {
+    checkWin();
+    checkIfTie();
+    if (player === "X") {
+      setPlayer("O")
+    } else {
+      setPlayer("X")
+    }
+  }, [board]);
+
+  useEffect(() => {
+    if (result.state !== 'none') {
+      alert(`Game Finished!  Winning Player: ${result.winner}`)
+      restartGame();
+    }
+  }, [result]);
+
+  function chooseSquare(square) {
+    setBoard(board.map((val, idx) => {
+      if (idx === square && val === "") {
+        return player
+      } 
+
+      return val;
+    }))
+  };
+
+  function checkWin() {
+    Patterns.forEach((currentPattern) => {
+      let firstPlayer = board[currentPattern[0]];
+      if (firstPlayer === "") return;
+      let foundWinningPattern = true;
+      currentPattern.forEach((idx) => {
+        if (board[idx] !== firstPlayer) {
+          foundWinningPattern = false;
+        }
+      })
+
+      if (foundWinningPattern) {
+        setResult({ winner: player, state: "WON!" })
+      }
+    })
+  }
+
+  function checkIfTie() {
+    let filled = true;
+    board.forEach((square) => {
+      if (square === "") {
+        filled = false
+      }
+    })
+
+    if (filled) {
+      setResult({ winner: "No One", state: "Tie" })
+    }
+  }
+
+  function restartGame() {
+    setBoard(['','','','','','','','','']);
+    setPlayer("O");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='board'>
+        <div className='row'>
+          <Square 
+            val={board[0]} 
+            chooseSquare={ () => {chooseSquare(0)} }
+          />
+          <Square 
+            val={board[1]} 
+            chooseSquare={ () => {chooseSquare(1)} }
+          />
+          <Square 
+            val={board[2]} 
+            chooseSquare={ () => {chooseSquare(2)} }
+          />
+        </div>
+        <div className='row'>
+          <Square 
+            val={board[3]} 
+            chooseSquare={ () => {chooseSquare(3)} }
+          />
+          <Square 
+            val={board[4]} 
+            chooseSquare={ () => {chooseSquare(4)} }
+          />
+          <Square 
+            val={board[5]} 
+            chooseSquare={ () => {chooseSquare(5)} }
+          />
+        </div>
+        <div className='row'>
+          <Square 
+            val={board[6]} 
+            chooseSquare={ () => {chooseSquare(6)} }
+          />
+          <Square 
+            val={board[7]} 
+            chooseSquare={ () => {chooseSquare(7)} }
+          />
+          <Square 
+            val={board[8]} 
+            chooseSquare={ () => {chooseSquare(8)} }
+          />
+        </div>
+      </div> 
     </div>
   );
 }
